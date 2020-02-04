56ead6d831bb2d80f409ec76c2a81d4e98225a88
Pierre Vanoverschelde <pierre.vano@gmail.com>
2020-02-05 00:03:03 +0100

    update code

--- deploytogithub.sh
+++ deploytogithub.sh
@@ -1,7 +1,10 @@
 commit=$1
 
-rm -f ../yaquoiaucinegithub/
-cp ./ ../yaquoiaucinegithub/
+mv ../yaquoiaucinegithub/.git ../
+rm -f -R ../yaquoiaucinegithub
+mkdir ../yaquoiaucinegithub
+mv ../.git ../yaquoiaucinegithub
+rsync -av --progress . ../yaquoiaucinegithub --exclude .git/
 cd ../yaquoiaucinegithub
 
 git add . && git commit -m "$commit" && git push github master
